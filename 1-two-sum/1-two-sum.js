5b32745a45c9c023cb6624a66e80029f97d6889c
dylandhood <66035537+dylanhood@users.noreply.github.com>
2022-06-21 00:16:01 -0500

    Time: 109 ms (58.58%), Space: 42.8 MB (39.07%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,20 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let p1 = 0; p1 < nums.length; p1++) {
-        const numberToFind = target - nums[p1];
+    const numsMap = {};
+    
+    for (let p = 0; p < nums.length; p++) {
+        const currentMapVal = numsMap[nums[p]];
         
-        for(let p2 = p1 + 1; p2 < nums.length; p1++) {
-            const numberToFind = target - nums[p1];
-            
-            for(let p2 = p1 + 1; p2 < nums.length; p2++) {
-                if(numberToFind === nums[p2]) {
-                    return [p1, p2];
-                }
-            }
+        if (currentMapVal >= 0) {
+            return [currentMapVal, p];
+        } else {
+            const numberToFind = target - nums[p];
+            numsMap[numberToFind] = p;
         }
     }
     
     return null;
-    
 };
