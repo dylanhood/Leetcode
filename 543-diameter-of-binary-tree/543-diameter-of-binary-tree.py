fc8d8917316a41789a5278e35c5a7be6c647e40e
dylandhood <66035537+dylanhood@users.noreply.github.com>
2022-07-19 16:02:02 -0500

    Time: 71 ms (50.76%), Space: 16.5 MB (10.03%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.py
@@ -13,7 +13,7 @@ class Solution:
                 return -1
             left = dfs(root.left)
             right = dfs(root.right)
-            res [0] = max(res [0], 2 + left + right)
+            res[0] = max(res[0], 2 + left + right)
             
             return 1 + max(left, right)
         
